5be8459129f3eedbfd96c1cf3524b95d72903665
sehaj anand <sehajanand.321@gmail.com>
2022-07-03 11:35:02 +0530

    added new code

--- circular_linked_list_and_operations.c
+++ circular_linked_list_and_operations.c
@@ -1,80 +1,113 @@
-#include <stdio.h>
-#include <stdlib.h>
+# include<stdio.h>
+# include<stdlib.h>
 
-struct node
-{
+struct node{
     int data;
     struct node *ptr;
 };
 
-void display(struct node *head)
-{
-    struct node *first = head;
-    do
-    {
-        printf("%d ", head->data);
-        head = head->ptr;
-    } while (head != first);
+void display(struct node *head){
+    struct node *headStore=head;
+    do{
+        printf("%d ",head->data);
+        head=head->ptr;
+    }while(head!=headStore);
     printf("\n");
-    ;
-};
-
-struct node *insertAtStart(struct node *head, int data)
-{
-    struct node *p = head;
+;};
+struct node * insertAtStart(struct node *head, int data){
+    struct node *headStore=head;
     struct node *new;
-    new = (struct node *)malloc(sizeof(struct node));
-    new->data = data;
-    new->ptr = head;
-    while (p->ptr != head)
-    {
-        p = p->ptr;
-    };
-    head = new;
-    p->ptr = new;
+    new=(struct node *)malloc(sizeof(struct node));
+    new->data=data;
+    new->ptr=head;
+    do{
+        headStore=headStore->ptr;
+    }while(headStore->ptr!=head);
+    head=new;
+    headStore->ptr=new;
     return head;
-    ;
-};
+;};
 
-struct node *insertInBetween(struct node *head, int data, int ind)
-{
+struct node* insertInBetween(struct node *head, int data, int ind){
     struct node *new;
-    new->data = data;
-    for (int i = 0; i < ind - 1; i++)
-    {
-        head = head->ptr;
+    new=(struct node *)malloc(sizeof(struct node *));
+    new->data=data;
+    for(int i=0;i<ind-1;i++){
+        head=head->ptr;
     };
-    new->ptr = head->ptr;
-    head->ptr = new;
+    new->ptr=head->ptr;
+    head->ptr=new;
     return new;
-    ;
-};
+;};
 
-int main()
-{
+struct node * insertAfterNode( struct node *ptr,int data){
+    struct node *new;
+    new=(struct node *)malloc(sizeof(struct node));
+    new->data=data;
+    new->ptr=ptr->ptr;
+    ptr->ptr=new;
+    return new;
+;};
+
+struct node* deleteAtStart(struct node *head){
+    struct node *headStore=head;
+    do{
+        head=head->ptr;
+    }while(head->ptr!=headStore);
+    struct node *nodeToBeDeleted=head->ptr;
+    head->ptr=headStore->ptr;
+    headStore=headStore->ptr;
+    free(nodeToBeDeleted);
+    return headStore;
+;};
+
+void deleteInBetween(struct node *head, int ind){
+    for(int i=0;i<ind-1;i++){
+        head=head->ptr;
+    };
+    struct node *nodeToBeDeleted=head->ptr;
+    head->ptr=(head->ptr)->ptr;
+    free(nodeToBeDeleted);
+;};
+
+void deleteAfterNode(struct node *ptr){
+    struct node *nodeToBeDeleted=ptr->ptr;
+    ptr->ptr=(ptr->ptr)->ptr;
+    free(nodeToBeDeleted);
+}
+
+int main(){
     struct node *head;
     struct node *first;
     struct node *second;
     struct node *third;
     struct node *fourth;
-    head = (struct node *)malloc(sizeof(struct node));
-    first = (struct node *)malloc(sizeof(struct node));
-    second = (struct node *)malloc(sizeof(struct node));
-    third = (struct node *)malloc(sizeof(struct node));
-    fourth = (struct node *)malloc(sizeof(struct node));
-    head = first;
-    first->data = 1;
-    first->ptr = second;
-    second->data = 2;
-    second->ptr = third;
-    third->data = 3;
-    third->ptr = fourth;
-    fourth->data = 4;
-    fourth->ptr = first;
+    head=(struct node *)malloc(sizeof(struct node));
+    first=(struct node *)malloc(sizeof(struct node));
+    second=(struct node *)malloc(sizeof(struct node));
+    third=(struct node *)malloc(sizeof(struct node));
+    fourth=(struct node *)malloc(sizeof(struct node));
+    head=first;
+    first->data=1;
+    first->ptr=second;
+    second->data=2;
+    second->ptr=third;
+    third->data=3;
+    third->ptr=fourth;
+    fourth->data=4;
+    fourth->ptr=first;
     display(head);
-    head = insertAtStart(head, 99);
+    head=insertAtStart(head,100);
     display(head);
-    insertInBetween(head, 1000, 5);
+    struct node *new=insertInBetween(head,99,5);
+    display(head);
+    struct node *new2=insertAfterNode(first,88);
+    display(head);
+    head=deleteAtStart(head);
+    display(head);
+    deleteInBetween(head,1);
+    display(head);
+    deleteAfterNode(second);
     display(head);
     return 0;
 }
