43dab5f459b3144cc3f5082811c9acba803b560e
sehaj anand <sehajanand.321@gmail.com>
2022-10-06 13:39:54 +0530

    modified code

--- linear_search_for_sorted_or_unsorted_array.c
+++ linear_search_for_sorted_or_unsorted_array.c
@@ -1,15 +1,73 @@
+// USING SIMPLE ARRAY SYNTAX:-
+
+// # include<stdio.h>
+// # include<stdlib.h>
+
+// int linearSearch(int a[],int uSize,int element){
+//     for(int i=0;i<uSize;i++){
+//         if(a[i]==element){
+//             return i;
+//         };
+//     };
+//     printf("Element not found.\n");
+// ;};
+
+// int main(){
+//     int arr[5]={1,2,3,4,5};
+//     int elementIndex=linearSearch(arr,5,1);
+//     printf("%d",elementIndex);
+//     return 0;
+// }
+
+// USING ARRAY ADT:-
+
 # include<stdio.h>
-int linearSearch(int a[], int size, int el){
-    for(int i=0;i<size;i++){
-        if(a[i]==el){
+# include<stdlib.h>
+
+struct myArray{
+    int totalSize;
+    int usedSize;
+    int *ptr;
+};
+
+struct myArray* createArray(int tSize){
+    struct myArray *ptr;
+    ptr=(struct myArray *)malloc(sizeof(struct myArray));
+    ptr->totalSize=tSize;
+    ptr->ptr=(int *)malloc(tSize*sizeof(int));
+    return ptr;
+;};
+    
+void setElements(struct myArray *ptr,int uSize){
+    ptr->usedSize=uSize;
+    for(int i=0;i<ptr->usedSize;i++){
+        printf("Enter value of element indexed %d of array.\n",i);
+        scanf("%d",&(ptr->ptr[i]));
+    };
+;};
+
+void displayArray(struct myArray *ptr){
+    printf("{");
+    for(int i=0;i<ptr->usedSize;i++){
+        printf("%d,",ptr->ptr[i]);
+    };
+    printf("}\n");
+;};
+
+int linearSearch(struct myArray *ptr,int element){
+    for(int i=0;i<ptr->usedSize;i++){
+        if(ptr->ptr[i]==element){
             return i;
         };
     };
-    return -1;
+    printf("Element not found.\n");
 ;};
 
 int main(){
-    int a[]={1,2,3,4,5,6,7,8};
-    printf("Element was found at %d.\n",linearSearch(a,8,900));
+    struct myArray *arr=createArray(10);
+    setElements(arr,5);
+    displayArray(arr);
+    int elementIndex=linearSearch(arr,2);
+    printf("%d",elementIndex);
     return 0;
 }
---
+++ rough.c
@@ -0,0 +1,50 @@
+# include<stdio.h>
+# include<stdlib.h>
+
+struct myArray{
+    int totalSize;
+    int usedSize;
+    int *ptr;
+};
+
+struct myArray* createArray(int tSize){
+    struct myArray *ptr;
+    ptr=(struct myArray *)malloc(sizeof(struct myArray));
+    ptr->totalSize=tSize;
+    ptr->ptr=(int *)malloc(tSize*sizeof(int));
+    return ptr;
+;};
+    
+void setElements(struct myArray *ptr,int uSize){
+    ptr->usedSize=uSize;
+    for(int i=0;i<ptr->usedSize;i++){
+        printf("Enter value of element indexed %d of array.\n",i);
+        scanf("%d",&(ptr->ptr[i]));
+    };
+;};
+
+void displayArray(struct myArray *ptr){
+    printf("{");
+    for(int i=0;i<ptr->usedSize;i++){
+        printf("%d,",ptr->ptr[i]);
+    };
+    printf("}\n");
+;};
+
+int linearSearch(struct myArray *ptr,int element){
+    for(int i=0;i<ptr->usedSize;i++){
+        if(ptr->ptr[i]==element){
+            return i;
+        };
+    };
+     printf("Element not found.\n");
+;};
+
+int main(){
+    struct myArray *arr=createArray(10);
+    setElements(arr,5);
+    displayArray(arr);
+    int elementIndex=linearSearch(arr,2);
+    printf("%d",elementIndex);
+    return 0;
+}
